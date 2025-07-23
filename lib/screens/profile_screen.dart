277714087f408313779fc6ede6d1242e5cde2569
denis.declercq <denis.declercq@adevinta.com>
2025-07-23 18:08:49 +0200

    fix(profile): Fix sharing functionality and update scores list
    This commit fixes the issues with the sharing functionality in the profile screen by adding the ShareParams and fixing the quotes.
    Updates the scores list to be more chibi with wooden panels and golden cups.

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,6 +10,9 @@ import 'package:oracle_d_asgard/models/myth_card.dart';
 import 'package:oracle_d_asgard/models/myth_story.dart';
 import 'package:oracle_d_asgard/models/collectible_card.dart';
 import 'package:oracle_d_asgard/data/stories_data.dart';
+import 'package:oracle_d_asgard/data/app_data.dart';
+import 'package:oracle_d_asgard/models/deity.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -52,20 +55,33 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text(
-          'Profil',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.displayMedium?.copyWith(
-            fontFamily: 'AmaticSC',
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 40,
-            letterSpacing: 2.0,
-            shadows: [const Shadow(blurRadius: 15.0, color: Colors.black87, offset: Offset(4.0, 4.0))],
-          ),
-        ),
         backgroundColor: Colors.transparent,
         elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.share, color: Colors.white),
+            onPressed: () async {
+              final gamificationService = Provider.of<GamificationService>(context, listen: false);
+              final quizResults = await gamificationService.getQuizResults();
+              if (quizResults.isNotEmpty) {
+                final lastQuizResult = quizResults.first;
+                final deityName = lastQuizResult['deity_name'];
+                final Deity? deity = AppData.deities[deityName.toLowerCase()];
+                if (deity != null) {
+                  SharePlus.instance.share(
+                    ShareParams(text: 'Mon résultat au quiz Oracle d\'Asgard : Je suis ${deity.name} ! Découvrez votre divinité sur l\'application !'),
+                  );
+                }
+              }
+            },
+          ),
+        ],
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -89,16 +105,77 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   return const Center(child: Text('No data available.'));
                 } else {
                   final List<Map<String, dynamic>> snakeScores = snapshot.data![0];
-                  final List<Map<String, dynamic>> unlockedTrophies =
-                      snapshot.data![1];
-                  final List<Map<String, dynamic>> unlockedCards =
-                      snapshot.data![2];
-                  final List<Map<String, dynamic>> storyProgress =
-                      snapshot.data![3];
+                  final List<Map<String, dynamic>> unlockedTrophies = snapshot.data![1];
+                  final List<Map<String, dynamic>> unlockedCards = snapshot.data![2];
+                  final List<Map<String, dynamic>> storyProgress = snapshot.data![3];
 
                   return ListView(
                     padding: const EdgeInsets.all(16.0),
                     children: [
+                      FutureBuilder<List<Map<String, dynamic>>>(
+                        future: gamificationService.getQuizResults(),
+                        builder: (context, quizSnapshot) {
+                          if (quizSnapshot.connectionState == ConnectionState.waiting) {
+                            return const Center(child: CircularProgressIndicator());
+                          } else if (quizSnapshot.hasError) {
+                            return Center(child: Text('Error loading quiz result: ${quizSnapshot.error}'));
+                          } else if (!quizSnapshot.hasData || quizSnapshot.data!.isEmpty) {
+                            return const SizedBox.shrink(); // No quiz results yet
+                          } else {
+                            final lastQuizResult = quizSnapshot.data!.first;
+                            final deityName = lastQuizResult['deity_name'];
+                            final Deity? deity = AppData.deities[deityName.toLowerCase()];
+
+                            if (deity == null) {
+                              return const SizedBox.shrink(); // Deity not found
+                            }
+
+                            return Column(
+                              children: [
+                                Text(
+                                  deity.name,
+                                  textAlign: TextAlign.center,
+                                  style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                                    fontFamily: 'AmaticSC',
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 70,
+                                    letterSpacing: 2.0,
+                                    shadows: [const Shadow(blurRadius: 15.0, color: Colors.black87, offset: Offset(4.0, 4.0))],
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                Container(
+                                  width: 150,
+                                  height: 150,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15), // Rounded corners for the button effect
+                                    border: Border.all(
+                                      color: const Color(0xFFDAA520), // Darker gold for the main border
+                                      width: 5,
+                                    ),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Color(0xFFDAA520), // Darker gold for bottom-right shadow
+                                        offset: Offset(5, 5),
+                                      ),
+                                      BoxShadow(
+                                        color: Color(0xFFFFD700), // Lighter gold for top-left highlight
+                                        offset: Offset(-5, -5),
+                                      ),
+                                    ],
+                                  ),
+                                  child: ClipRRect(
+                                    // Use ClipRRect for rounded rectangle
+                                    borderRadius: BorderRadius.circular(10), // Slightly smaller radius for inner image
+                                    child: Image.asset(deity.icon, fit: BoxFit.cover),
+                                  ),
+                                ),
+                              ],
+                            );
+                          }
+                        },
+                      ),
                       _buildSectionTitle('Scores de jeu'),
                       _buildGameScores(snakeScores),
                       const SizedBox(height: 20),
@@ -159,8 +236,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     } else if (difference.inDays < 30) {
       final weeks = (difference.inDays / 7).floor();
       return "Il y a $weeks sem";
-    }
-    else if (difference.inDays < 365) {
+    } else if (difference.inDays < 365) {
       final months = (difference.inDays / 30).floor();
       return "Il y a $months mois";
     } else {
@@ -173,11 +249,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     if (scores.isEmpty) {
       return Text(
         'Aucun score de Snake pour l’instant.',
-        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          color: Colors.white70,
-          fontFamily: 'AmaticSC',
-          fontSize: 20,
-        ),
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white70, fontFamily: 'AmaticSC', fontSize: 20),
       );
     }
     return Column(
@@ -194,50 +266,45 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ),
         const SizedBox(height: 10),
-        ...scores.map(
-          (score) {
-            final DateTime timestamp =
-                DateTime.fromMillisecondsSinceEpoch(score['timestamp']);
-            return Card(
-              color: Colors.white.withAlpha(25),
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              margin: const EdgeInsets.symmetric(vertical: 5),
+        ...scores.map((score) {
+          final DateTime timestamp = DateTime.fromMillisecondsSinceEpoch(score['timestamp']);
+          return Card(
+            color: Colors.brown[700], // Placeholder for wood texture
+            elevation: 5,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
               child: Row(
                 children: [
-                  const Icon(Icons.score, color: Colors.greenAccent, size: 24),
-                  const SizedBox(width: 10),
+                  Icon(Icons.emoji_events, color: Colors.amber, size: 36), // Golden cup icon
+                  const SizedBox(width: 15),
                   Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Score: ${score['score']}',
-                            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'AmaticSC',
-                              fontSize: 20,
-                            ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Score: ${score['score']}',
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'AmaticSC',
+                            fontSize: 22,
                           ),
-                          const SizedBox(height: 4),
-                          Text(
-                            'Date: ${_formatRelativeTime(timestamp)}',
-                            style: const TextStyle(
-                                color: Colors.white70, fontSize: 12),
-                          ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Date: ${_formatRelativeTime(timestamp)}',
+                          style: const TextStyle(color: Colors.white70, fontSize: 14),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            );
-          },
-        ),
+            ),
+          );
+        }),
       ],
     );
   }
@@ -246,31 +313,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
     if (trophies.isEmpty) {
       return Text(
         'Aucun trophée débloqué pour l’instant.',
-        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          color: Colors.white70,
-          fontFamily: 'AmaticSC',
-          fontSize: 20,
-        ),
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white70, fontFamily: 'AmaticSC', fontSize: 20),
       );
     }
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 0.8,
-      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10, childAspectRatio: 0.8),
       itemCount: trophies.length,
       itemBuilder: (context, index) {
         final trophy = trophies[index];
         return Card(
           color: Colors.white.withAlpha(25),
           elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -279,12 +335,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
               Text(
                 trophy['trophy_id'],
                 textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'AmaticSC',
-                  fontSize: 20,
-                ),
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white, fontWeight: FontWeight.bold, fontFamily: 'AmaticSC', fontSize: 20),
               ),
             ],
           ),
@@ -297,22 +348,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
     if (cards.isEmpty) {
       return Text(
         'Aucune carte à collectionner débloquée pour l’instant.',
-        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          color: Colors.white70,
-          fontFamily: 'AmaticSC',
-          fontSize: 20,
-        ),
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white70, fontFamily: 'AmaticSC', fontSize: 20),
       );
     }
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 1.0,
-      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10, childAspectRatio: 1.0),
       itemCount: cards.length,
       itemBuilder: (context, index) {
         final cardData = cards[index];
@@ -331,11 +373,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     if (storyProgress.isEmpty) {
       return Text(
         'Aucune histoire débloquée pour l’instant.',
-        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-          color: Colors.white70,
-          fontFamily: 'AmaticSC',
-          fontSize: 20,
-        ),
+        style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white70, fontFamily: 'AmaticSC', fontSize: 20),
       );
     }
     final allMythStories = getMythStories();
@@ -343,12 +381,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 1.0,
-      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10, childAspectRatio: 1.0),
       itemCount: storyProgress.length,
       itemBuilder: (context, index) {
         final progress = storyProgress[index];
@@ -363,49 +396,28 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
         return GestureDetector(
           onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MythStoryPage(mythStory: mythStory),
-              ),
-            );
+            Navigator.push(context, MaterialPageRoute(builder: (context) => MythStoryPage(mythStory: mythStory)));
           },
           child: Card(
             color: Colors.white.withAlpha(25),
             elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.book,
-                  color: Colors.purpleAccent,
-                  size: 40,
-                ), // Placeholder icon
+                Icon(Icons.book, color: Colors.purpleAccent, size: 40), // Placeholder icon
                 const SizedBox(height: 8),
                 Text(
                   mythStory.title,
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'AmaticSC',
-                    fontSize: 20,
-                  ),
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyLarge?.copyWith(color: Colors.white, fontWeight: FontWeight.bold, fontFamily: 'AmaticSC', fontSize: 20),
                 ),
                 const SizedBox(height: 8),
-                LinearProgressIndicator(
-                  value: progressPercentage,
-                  backgroundColor: Colors.grey[700],
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
-                ),
+                LinearProgressIndicator(value: progressPercentage, backgroundColor: Colors.grey[700], valueColor: AlwaysStoppedAnimation<Color>(Colors.amber)),
                 const SizedBox(height: 4),
-                Text(
-                  '${(progressPercentage * 100).toStringAsFixed(0)}% Terminée',
-                  style: TextStyle(color: Colors.white70, fontSize: 12),
-                )
+                Text('${(progressPercentage * 100).toStringAsFixed(0)}% Terminée', style: TextStyle(color: Colors.white70, fontSize: 12)),
               ],
             ),
           ),
